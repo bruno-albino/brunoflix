896e485c47a013b8a9b17d7b1d1a041c78b69e5d
Brunoalbino97 <bruno-albino@hotmail.com>
2020-08-01 12:45:58 -0300

    commit test 4

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const PORT = process.env.PORT || 8080
-
 server.use(router)
 server.use(middlewares)
-server.listen(PORT, () => {
-    console.log('JSON server is running on PORT: ', PORT)
-})
+server.listen(process.env.PORT || 5000)
